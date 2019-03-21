380bbede9b9e1b94947ba6c463a8e938426a0205
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-03-21 11:11:34 +0700

    add ts

---
+++ components/uikit/Pagination.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.pagination
+    | pagination_content
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+
+export default class Pagination extends Vue {
+    @Prop({
+        required: true,
+        type: Number
+    })
+    currentPage!: number;
+};
+</script>
+
