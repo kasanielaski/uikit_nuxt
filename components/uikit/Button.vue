71c565af8d22a95abb005c48b14ba6e856d0c913
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-03-27 11:44:55 +0700

    add button component

---
+++ components/uikit/Button.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+button.button(
+    :class="classes",
+    @click="onClick"
+)
+    slot(name="icon")
+    slot(name="text")
+</template>
+
+<script>
+export default {
+    name: 'Button',
+
+    props: {
+        size: {
+            required: false,
+            type: String,
+            default: 'medium'
+        }
+    },
+
+    computed: {
+        classes() {
+            const classes = [];
+            classes.push(`_${this.size}`);
+
+            return classes;
+        }
+    },
+
+    methods: {
+        onClick() {
+            this.$emit('onClick');
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+    &._small {
+        height: 18px;
+    }
+
+    &._medium {
+        height: 24px;
+    }
+
+    &._big {
+        height: 36px;
+    }
+}
+</style>
